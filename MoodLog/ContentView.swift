8d68e24a6fa08e4ecc367f39d2994af8c4800e20
Ignacio Lagos <ilagosmo@gmail.com>
2026-03-27 19:51:13 -0300

    Initial Commit

---
+++ MoodLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoodLog
+//
+//  Created by Ignacio Lagos Morales on 27-03-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoodLog/MoodLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodLogApp.swift
+//  MoodLog
+//
+//  Created by Ignacio Lagos Morales on 27-03-26.
+//
+
+import SwiftUI
+
+@main
+struct MoodLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
